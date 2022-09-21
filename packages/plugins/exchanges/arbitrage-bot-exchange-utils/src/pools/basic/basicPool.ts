8664a79c049837bea5f249d233f73ccdda56238c
dexterslabor <dexterslabor@users.noreply.github.com>
2022-09-21 22:57:38 +0200

    feat(exchange-utils): add getAmountInGivenOut for CFMM x*y=k

--- packages/plugins/exchanges/arbitrage-bot-exchange-utils/src/pools/basic/basicPool.ts
+++ packages/plugins/exchanges/arbitrage-bot-exchange-utils/src/pools/basic/basicPool.ts
@@ -1,7 +1,27 @@
+import { BigNumber } from 'bignumber.js';
+import { basisPoints } from '../../constants';
 // Uniswap V2 CFMM a.k.a. x*y=k
 
-export const calculateAmountInExactOut = () => {
-    return;
+/**
+ * Example for fee = 0.03% which is 3 BPS out of 1000 BPS
+ * amountIn = (amountOut * 1000 * reserveIn) / (reserveOut * 1000 - amountOut * 997)
+ */
+export const getAmountInGivenOut = (
+    amountOut: string,
+    reserveIn: string,
+    reserveOut: string,
+    fee: number
+) => {
+    const amountOutBN = new BigNumber(amountOut);
+
+    const numerator = amountOutBN
+        .multipliedBy(basisPoints)
+        .multipliedBy(reserveIn);
+    const denominator = new BigNumber(reserveOut)
+        .multipliedBy(basisPoints)
+        .minus(amountOutBN.multipliedBy(basisPoints - fee));
+
+    return numerator.idiv(denominator);
 };
 
 export const calculateAmountOutExactIn = () => {
