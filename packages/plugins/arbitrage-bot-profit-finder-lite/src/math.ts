48b043c0afcb1e14f4156e8e7846b33b6b48b6f8
dexterslabor <dexterslabor@users.noreply.github.com>
2022-09-27 17:24:47 +0200

    test(profit-finder): update math test for precision

--- packages/plugins/arbitrage-bot-profit-finder-lite/src/math.ts
+++ packages/plugins/arbitrage-bot-profit-finder-lite/src/math.ts
@@ -1,7 +1,7 @@
 import { ExchangePrice, Token, TokenDecimals } from '@stove-labs/arbitrage-bot';
 import {
-    getAmountInGivenOut,
-    getAmountOutGivenIn,
+  getAmountInGivenOut,
+  getAmountOutGivenIn,
 } from './pools/basic/basicPool';
 import { BigNumber } from 'bignumber.js';
 import _ from 'lodash';
@@ -13,26 +13,26 @@ import _ from 'lodash';
  * receive 1 USD
  */
 export const addSpotPrice = (
-    prices: ExchangePrice[],
-    tokenDecimals: TokenDecimals
+  prices: ExchangePrice[],
+  tokenDecimals: TokenDecimals
 ): ExchangePrice[] => {
-    prices.map((exchangePrice) => {
-        const baseTokenReserve = new BigNumber(
-            exchangePrice.baseTokenBalance.amount
-        ).dividedBy(10 ** tokenDecimals.baseToken);
+  prices.map((exchangePrice) => {
+    const baseTokenReserve = new BigNumber(
+      exchangePrice.baseTokenBalance.amount
+    ).dividedBy(10 ** tokenDecimals.baseToken);
 
-        const quoteTokenReserve = new BigNumber(
-            exchangePrice.quoteTokenBalance.amount
-        ).dividedBy(10 ** tokenDecimals.quoteToken);
+    const quoteTokenReserve = new BigNumber(
+      exchangePrice.quoteTokenBalance.amount
+    ).dividedBy(10 ** tokenDecimals.quoteToken);
 
-        exchangePrice.spotPrice = baseTokenReserve
-            .dividedBy(quoteTokenReserve)
-            .multipliedBy(10 ** tokenDecimals.baseToken)
-            .toFixed(0, 1);
+    exchangePrice.spotPrice = baseTokenReserve
+      .dividedBy(quoteTokenReserve)
+      .multipliedBy(10 ** tokenDecimals.baseToken)
+      .toFixed(0, 1);
 
-        return exchangePrice;
-    });
-    return prices;
+    return exchangePrice;
+  });
+  return prices;
 };
 
 /**
@@ -44,39 +44,39 @@ export const addSpotPrice = (
  * Spot price: 1USD = 0.5 XTZ
  */
 export const orderLowToHigh = (
-    prices: ExchangePrice[],
-    tokenDecimals: TokenDecimals
+  prices: ExchangePrice[],
+  tokenDecimals: TokenDecimals
 ): ExchangePrice[] => {
-    //const tokenInfo = getTokenInfo(prices);
-    prices = addSpotPrice(prices, tokenDecimals);
+  //const tokenInfo = getTokenInfo(prices);
+  prices = addSpotPrice(prices, tokenDecimals);
 
-    return _.orderBy(
-        prices,
-        [
-            // need to transform string to number so it can be ordered
-            (exchangePrice) => {
-                return new BigNumber(exchangePrice.spotPrice).toNumber();
-            },
-        ],
-        'asc'
-    );
+  return _.orderBy(
+    prices,
+    [
+      // need to transform string to number so it can be ordered
+      (exchangePrice) => {
+        return new BigNumber(exchangePrice.spotPrice).toNumber();
+      },
+    ],
+    'asc'
+  );
 };
 
 type SwapBuy = {
-    reserveIn: string;
-    reserveOut: string;
-    tokenIn: Token;
-    tokenOut: Token;
+  reserveIn: string;
+  reserveOut: string;
+  tokenIn: Token;
+  tokenOut: Token;
 };
 type SwapSell = {
-    reserveIn: string;
-    reserveOut: string;
-    tokenIn: Token;
-    tokenOut: Token;
+  reserveIn: string;
+  reserveOut: string;
+  tokenIn: Token;
+  tokenOut: Token;
 };
 export type TradingStrategy = {
-    swapBuy: SwapBuy;
-    swapSell: SwapSell;
+  swapBuy: SwapBuy;
+  swapSell: SwapSell;
 };
 
 /**
@@ -84,62 +84,66 @@ export type TradingStrategy = {
  * SpotPrice 0.2 quoteToken/baseToken USD/XTZ
  */
 export const splitIntoBuySell = (prices: ExchangePrice[]): TradingStrategy => {
-    // spot price lower => amountInGivenOut => BUY
-    const exchangeBuy = {
-        reserveIn: prices[0].baseTokenBalance.amount,
-        reserveOut: prices[0].quoteTokenBalance.amount,
-        tokenIn: prices[0].baseToken,
-        tokenOut: prices[0].quoteToken,
-    } as SwapBuy;
-    // spot price higher => amountOutGivenIn => SELL
-    const exchangeSell = {
-        // notice that quote and base token are flipped here!
-        reserveIn: prices[1].quoteTokenBalance.amount,
-        reserveOut: prices[1].baseTokenBalance.amount,
-        tokenIn: prices[1].quoteToken,
-        tokenOut: prices[1].baseToken,
-    } as SwapSell;
-    return { swapBuy: exchangeBuy, swapSell: exchangeSell };
+  // spot price lower => amountInGivenOut => BUY
+  const exchangeBuy = {
+    reserveIn: prices[0].baseTokenBalance.amount,
+    reserveOut: prices[0].quoteTokenBalance.amount,
+    tokenIn: prices[0].baseToken,
+    tokenOut: prices[0].quoteToken,
+  } as SwapBuy;
+  // spot price higher => amountOutGivenIn => SELL
+  const exchangeSell = {
+    // notice that quote and base token are flipped here!
+    reserveIn: prices[1].quoteTokenBalance.amount,
+    reserveOut: prices[1].baseTokenBalance.amount,
+    tokenIn: prices[1].quoteToken,
+    tokenOut: prices[1].baseToken,
+  } as SwapSell;
+  return { swapBuy: exchangeBuy, swapSell: exchangeSell };
 };
 
 /**
  * Units:
  * buy.tokenOut = sell.tokenIn [token amount]
  */
-export const findOptimalAmount = (dex: TradingStrategy): BigNumber => {
-    // Univariate quadratic function with coefficients a,b,c f(x)=a*x^2+b*x+c=0
-    const { a, b, c } = extractCoefficientsOfUnivariateQuadraticFunction(dex);
+export const findOptimalQuoteTokenAmount = (
+  dex: TradingStrategy,
+  tokenDecimals: TokenDecimals
+): string => {
+  // Univariate quadratic function with coefficients a,b,c f(x)=a*x^2+b*x+c=0
+  const { a, b, c } = extractCoefficientsOfUnivariateQuadraticFunction(dex);
 
-    // Solving the quadratic function yields x1 and x2
-    // x1 = (- b + sqrt(b² - 4*a*c))/(2*a)
-    // x2 = (- b - sqrt(b² - 4*a*c))/(2*a)
-    const valueInSquareRoot = b
-        .pow(2)
-        .minus(new BigNumber(4).multipliedBy(a).multipliedBy(c));
-    // solving for x1
-    const x1 = b
-        .negated()
-        .plus(valueInSquareRoot.squareRoot())
-        .dividedBy(new BigNumber(2).multipliedBy(a));
-    // solving for x2
-    const x2 = b
-        .negated()
-        .minus(valueInSquareRoot.squareRoot())
-        .dividedBy(new BigNumber(2).multipliedBy(a));
-    // pick x based on constraints
-    let x;
-    if (x1.isPositive() && x1.isLessThan(dex.swapBuy.reserveOut)) {
-        x = x1;
-    } else if (
-        x2.isPositive() &&
-        x2.isLessThan(dex.swapBuy.reserveOut) &&
-        x2.isGreaterThan(x1)
-    ) {
-        x = x2;
-    } else {
-        x = new BigNumber(0);
-    }
-    return x;
+  // Solving the quadratic function yields x1 and x2
+  // x1 = (- b + sqrt(b² - 4*a*c))/(2*a)
+  // x2 = (- b - sqrt(b² - 4*a*c))/(2*a)
+  const valueInSquareRoot = b
+    .pow(2)
+    .minus(new BigNumber(4).multipliedBy(a).multipliedBy(c));
+  // solving for x1
+  const x1 = b
+    .negated()
+    .plus(valueInSquareRoot.squareRoot())
+    .dividedBy(new BigNumber(2).multipliedBy(a));
+  // solving for x2
+  const x2 = b
+    .negated()
+    .minus(valueInSquareRoot.squareRoot())
+    .dividedBy(new BigNumber(2).multipliedBy(a));
+    console.log('x1', x1.toString(), 'x2', x2.toString())
+  // pick x based on constraints
+  let x: BigNumber;
+  if (x1.isPositive() && x1.isLessThan(dex.swapBuy.reserveOut)) {
+    x = x1;
+  } else if (
+    x2.isPositive() &&
+    x2.isLessThan(dex.swapBuy.reserveOut) &&
+    x2.isGreaterThan(x1)
+  ) {
+    x = x2;
+  } else {
+    x = new BigNumber(0);
+  }
+  return x.multipliedBy(10 ** tokenDecimals.quoteToken).toFixed(0, 1);
 };
 
 // https://www.notion.so/stove-labs/Profit-Finder-d5844bc7e4ec4db48b2bfe395d42c256#a13825caae334a9299d86ef06801709d
@@ -148,56 +152,54 @@ export const findOptimalAmount = (dex: TradingStrategy): BigNumber => {
  * OptimalAmount is quoteToken
  */
 export const expectedProfitWithFees = (
-    optimalAmount: string,
-    reserveInSell: string,
-    reserveOutSell: string,
-    reserveInBuy: string,
-    reserveOutBuy: string,
-    fee: number
+  optimalAmount: string,
+  reserveInSell: string,
+  reserveOutSell: string,
+  reserveInBuy: string,
+  reserveOutBuy: string,
+  fee: number
 ) => {
-    const profit = getAmountOutGivenIn(
-        optimalAmount,
-        reserveOutSell,
-        reserveInSell,
-        fee
-    ).minus(
-        getAmountInGivenOut(optimalAmount, reserveInBuy, reserveOutBuy, fee)
-    );
-    return profit;
+  const profit = getAmountOutGivenIn(
+    optimalAmount,
+    reserveOutSell,
+    reserveInSell,
+    fee
+  ).minus(getAmountInGivenOut(optimalAmount, reserveInBuy, reserveOutBuy, fee));
+  return profit;
 };
 
 function extractCoefficientsOfUnivariateQuadraticFunction(
-    dex: TradingStrategy
+  dex: TradingStrategy
 ) {
-    // calculate a
-    const productBuyReserves = new BigNumber(
-        dex.swapBuy.reserveIn
-    ).multipliedBy(dex.swapBuy.reserveOut);
-    const productSellReserves = new BigNumber(
-        dex.swapSell.reserveIn
-    ).multipliedBy(dex.swapSell.reserveOut);
-    const a = productBuyReserves.minus(productSellReserves);
+  // calculate a
+  const productBuyReserves = new BigNumber(dex.swapBuy.reserveIn).multipliedBy(
+    dex.swapBuy.reserveOut
+  );
+  const productSellReserves = new BigNumber(
+    dex.swapSell.reserveIn
+  ).multipliedBy(dex.swapSell.reserveOut);
+  const a = productBuyReserves.minus(productSellReserves);
 
-    // calculate b
-    const b = new BigNumber(2)
-        .multipliedBy(dex.swapBuy.reserveOut)
+  // calculate b
+  const b = new BigNumber(2)
+    .multipliedBy(dex.swapBuy.reserveOut)
+    .multipliedBy(dex.swapSell.reserveOut)
+    .multipliedBy(
+      new BigNumber(dex.swapBuy.reserveIn).plus(dex.swapSell.reserveIn)
+    );
+
+  // calculate c
+  const c = new BigNumber(dex.swapBuy.reserveOut)
+    .multipliedBy(dex.swapSell.reserveOut)
+    .multipliedBy(
+      new BigNumber(dex.swapBuy.reserveIn)
         .multipliedBy(dex.swapSell.reserveOut)
-        .multipliedBy(
-            new BigNumber(dex.swapBuy.reserveIn).plus(dex.swapSell.reserveIn)
-        );
+        .minus(
+          new BigNumber(dex.swapSell.reserveIn).multipliedBy(
+            dex.swapBuy.reserveOut
+          )
+        )
+    );
 
-    // calculate c
-    const c = new BigNumber(dex.swapBuy.reserveOut)
-        .multipliedBy(dex.swapSell.reserveOut)
-        .multipliedBy(
-            new BigNumber(dex.swapBuy.reserveIn)
-                .multipliedBy(dex.swapSell.reserveOut)
-                .minus(
-                    new BigNumber(dex.swapSell.reserveIn).multipliedBy(
-                        dex.swapBuy.reserveOut
-                    )
-                )
-        );
-
-    return { a, b, c };
+  return { a, b, c };
 }
--- packages/plugins/arbitrage-bot-profit-finder-lite/test/math.test.ts
+++ packages/plugins/arbitrage-bot-profit-finder-lite/test/math.test.ts
@@ -1,92 +1,95 @@
 import { expect } from 'chai';
 
-import { ExchangePrice } from '@stove-labs/arbitrage-bot';
+import { ExchangePrice, TokenDecimals } from '@stove-labs/arbitrage-bot';
 import {
-    addSpotPrice,
-    findOptimalAmount,
-    orderLowToHigh,
-    TradingStrategy,
+  addSpotPrice,
+  findOptimalQuoteTokenAmount,
+  orderLowToHigh,
+  TradingStrategy,
 } from '../src/math';
 import { BigNumber } from 'bignumber.js';
 
 describe('math.ts', () => {
-    const prices = [
-        {
-            baseTokenBalance: { amount: '12470264030' },
-            quoteTokenBalance: { amount: '19328447920594343' },
-            exchangeIdentifier: 'exchange1',
-        },
-        {
-            baseTokenBalance: { amount: '100' }, // 0.000100
-            quoteTokenBalance: { amount: '400' }, // 0.000000000400
-            exchangeIdentifier: 'exchange2',
-        },
-        {
-            baseTokenBalance: { amount: '1000000' }, // 1.000000
-            quoteTokenBalance: { amount: '4000000000000' }, // 4.000000000000
-            exchangeIdentifier: 'exchange3',
-        },
-    ] as ExchangePrice[];
-    const spotPrices = [
-        '645176', // https://www.calculator.net/big-number-calculator.html?cx=12470.264030&cy=19328.447920594343&cp=30&co=divide * 60000 (decimals)
-        '250000000000', // https://www.calculator.net/big-number-calculator.html?cx=0.000100&cy=0.000000000400&cp=30&co=divide
-        '250000', // 0.25 at 6 decimals
-    ];
+  const prices = [
+    {
+      baseTokenBalance: { amount: '12470264030' },
+      quoteTokenBalance: { amount: '19328447920594343' },
+      exchangeIdentifier: 'exchange1',
+    },
+    {
+      baseTokenBalance: { amount: '100' }, // 0.000100
+      quoteTokenBalance: { amount: '400' }, // 0.000000000400
+      exchangeIdentifier: 'exchange2',
+    },
+    {
+      baseTokenBalance: { amount: '1000000' }, // 1.000000
+      quoteTokenBalance: { amount: '4000000000000' }, // 4.000000000000
+      exchangeIdentifier: 'exchange3',
+    },
+  ] as ExchangePrice[];
 
-    const tokenDecimals = { baseToken: 6, quoteToken: 12 };
+  // compare computed values to these
+  const spotPrices = [
+    '645176', // https://www.calculator.net/big-number-calculator.html?cx=12470.264030&cy=19328.447920594343&cp=30&co=divide * 60000 (decimals)
+    '250000000000', // https://www.calculator.net/big-number-calculator.html?cx=0.000100&cy=0.000000000400&cp=30&co=divide
+    '250000', // 0.25 at 6 decimals
+  ];
 
-    describe('addSpotPrice', () => {
-        it('can calculate and add spot price', () => {
-            const pricesWithSpotPrice = addSpotPrice(prices, tokenDecimals);
-            pricesWithSpotPrice.forEach((price, index) => {
-                expect(price.spotPrice).to.equal(spotPrices[index]);
-            });
-        });
+  const tokenDecimals = { baseToken: 6, quoteToken: 12 } as TokenDecimals;
+
+  describe('addSpotPrice', () => {
+    it('can calculate and add spot price', () => {
+      const pricesWithSpotPrice = addSpotPrice(prices, tokenDecimals);
+      pricesWithSpotPrice.forEach((price, index) => {
+        expect(price.spotPrice).to.equal(spotPrices[index]);
+      });
+    });
+  });
+
+  describe('orderLowToHigh', () => {
+    let listOfPrices = [prices[1], prices[0]];
+    let ascendingPrices;
+    beforeEach(() => {
+      ascendingPrices = orderLowToHigh(listOfPrices, tokenDecimals);
     });
 
-    describe('orderLowToHigh', () => {
-        let listOfPrices = [prices[1], prices[0]];
-        let ascendingPrices;
-        beforeEach(() => {
-            ascendingPrices = orderLowToHigh(listOfPrices, tokenDecimals);
-        });
-
-        it('can order spot prices in ascending order', () => {
-            expect(
-                Number(ascendingPrices[ascendingPrices.length - 1].spotPrice)
-            ).to.be.gt(Number(ascendingPrices[0].spotPrice));
-        });
-
-        it('can shift prices in an unordered list', () => {
-            expect(ascendingPrices[0]).to.equal(listOfPrices[1]);
-            expect(ascendingPrices[0]).to.not.equal(listOfPrices[0]);
-
-            expect(ascendingPrices[1]).to.equal(listOfPrices[0]);
-            expect(ascendingPrices[1]).to.not.equal(listOfPrices[1]);
-        });
-        it('can order a list of exchange prices based on ASCENDING spot price', () => {});
+    it('can order spot prices in ascending order', () => {
+      expect(
+        Number(ascendingPrices[ascendingPrices.length - 1].spotPrice)
+      ).to.be.gt(Number(ascendingPrices[0].spotPrice));
     });
-    describe('findOptimalQuoteTokenAmount', () => {
-        it('can calculate optimal amount', () => {
-            const tradingStrategy: TradingStrategy = {
-                swapBuy: {
-                    reserveIn: '12470',
-                    reserveOut: '19328',
-                    tokenIn: { ticker: 'XTZ' },
-                    tokenOut: { ticker: 'uUSD' },
-                },
-                swapSell: {
-                    reserveIn: '50125',
-                    reserveOut: '75942',
-                    tokenIn: { ticker: 'XTZ' },
-                    tokenOut: { ticker: 'uUSD' },
-                },
-            };
-            const optimalAmount = findOptimalAmount(tradingStrategy);
-            // const swap = createSwap(tradingStrategy, optimalAmount)
 
-            expect(optimalAmount.toFixed(4)).to.equal('174.8826');
-            // 
-        });
+    it('can shift prices in an unordered list', () => {
+      expect(ascendingPrices[0]).to.equal(listOfPrices[1]);
+      expect(ascendingPrices[0]).to.not.equal(listOfPrices[0]);
+
+      expect(ascendingPrices[1]).to.equal(listOfPrices[0]);
+      expect(ascendingPrices[1]).to.not.equal(listOfPrices[1]);
     });
+  });
+
+  describe('findOptimalQuoteTokenAmount', () => {
+    it.only('can calculate optimal quote token amount', () => {
+      const tradingStrategy: TradingStrategy = {
+        swapBuy: {
+          reserveIn: '12470',
+          reserveOut: '19328',
+          tokenIn: { ticker: 'XTZ' },
+          tokenOut: { ticker: 'uUSD' },
+        },
+        swapSell: {
+          reserveIn: '50125',
+          reserveOut: '75942',
+          tokenIn: { ticker: 'XTZ' },
+          tokenOut: { ticker: 'uUSD' },
+        },
+      };
+      const optimalAmount = findOptimalQuoteTokenAmount(
+        tradingStrategy,
+        tokenDecimals
+      );
+      
+      expect(optimalAmount).to.equal('174882610223218');
+    });
+  });
 });
