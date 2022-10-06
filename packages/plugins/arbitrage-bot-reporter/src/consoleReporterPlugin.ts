40e7c72ffb15c02473f3d1ab67cd80c7d620ddd2
dexterslabor <dexterslabor@users.noreply.github.com>
2022-10-06 11:55:52 +0200

    feat(reporter): add profit found support

--- packages/plugins/arbitrage-bot-reporter/src/consoleReporterPlugin.ts
+++ packages/plugins/arbitrage-bot-reporter/src/consoleReporterPlugin.ts
@@ -1,8 +1,28 @@
 import { ReporterPlugin, ReporterPluginEvent } from '@stove-labs/arbitrage-bot';
+import { Logger } from 'tslog';
+import chalk from 'chalk';
+import { handleProfitFound } from './events/profitFound';
+
+export const green = chalk.green;
+export const warning = chalk.yellow;
+export const blue = chalk.blue;
+export const red = chalk.red;
+
+
 
 export class ConsoleReporterPlugin implements ReporterPlugin {
+  log: Logger;
+  constructor() {
+    this.log = new Logger();
+    this.log.setSettings({
+      dateTimeTimezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+    });
+  }
   report(event: ReporterPluginEvent) {
-    // pretty print would be nice
-    console.log(event);
+    if (event.type === 'PROFIT_FOUND') {
+     handleProfitFound(event.profitOpportunity)
+    } else {
+      this.log.info(event);
+    }
   }
 }
