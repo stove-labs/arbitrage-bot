699685d6210e5906c4e590dfa99da17d699b7c98
dexterslabor <dexterslabor@users.noreply.github.com>
2022-10-16 14:18:49 +0200

    feat(exchange-utils): method to find trading pairs on multiple exchanges

--- packages/plugins/exchanges/arbitrage-bot-exchange-utils/src/arbitrageBotExchangeUtils.ts
+++ packages/plugins/exchanges/arbitrage-bot-exchange-utils/src/arbitrageBotExchangeUtils.ts
@@ -1,4 +1,5 @@
 import { Token, ExchangeRegistry } from '@stove-labs/arbitrage-bot';
+import _ from 'lodash';
 
 export const getExchangeAddressFromRegistry = (
   token1: Token,
@@ -16,4 +17,48 @@ export const getExchangeAddressFromRegistry = (
     });
     return exchangeInstance ? exchangeInstance.address : undefined;
   }
+};
+
+/**
+ * This method returns all trading pairs that are being traded
+ * on two or more exchanges.
+ * Ignore those trading pairs that are not being traded on at least 2 exchanges,
+ * because no arbitrage would be possible.
+ */
+export const getDuplicateTradingPairsFromAllExchanges = (
+  exchangeRegistry: ExchangeRegistry[]
+) => {
+  // extract all tickers [[ticker1, ticker2], [ticker1, ticker2], ...]
+  const pairs = exchangeRegistry.flatMap((exchanges) =>
+    exchanges.map((pair) => [pair.ticker1, pair.ticker2])
+  );
+  // order alphabetically eg. => [ticker2, ticker1]
+  const pairsSorted = pairs.map((pair) =>
+    pair.sort((a, b) => a.localeCompare(b))
+  );
+  // convert into an object and add an id
+  const sortedPairsObject = pairsSorted.map((pair) => {
+    const ticker1 = pair[0];
+    const ticker2 = pair[1];
+    const id = `${ticker1}${ticker2}`;
+
+    return {
+      ticker1,
+      ticker2,
+      id,
+    };
+  });
+
+  const tickers = _.flow([
+    (values) => _.groupBy(values, 'id'), // group elements by id
+    // take only duplicates or more, thus remove uniques
+    (grouped) => _.filter(grouped, (o) => o.length > 1),
+    _.flatten, // flatten the results to a single array
+    (b) => _.uniqBy(b, 'id'), // extract unique
+  ])(sortedPairsObject);
+
+  // remove id
+  tickers.forEach((tickersExchange) => delete tickersExchange.id);
+
+  return tickers;
 };
