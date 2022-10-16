ca180424b3e70f2f6cbcdfc9eec63a03d56463c8
dexterslabor <dexterslabor@users.noreply.github.com>
2022-10-16 14:31:39 +0200

    feat(core-cli): add spinner, yargv

--- packages/arbitrage-bot-cli/src/config.example.arbitrage-bot.ts
+++ packages/arbitrage-bot-cli/src/config.example.arbitrage-bot.ts
@@ -9,17 +9,18 @@ import { TriggerIntervalPlugin } from '@stove-labs/arbitrage-bot-trigger';
 import { ConsoleReporterPlugin } from '@stove-labs/arbitrage-bot-reporter';
 import { ProfitFinderLitePlugin } from '@stove-labs/arbitrage-bot-profit-finder-lite';
 import { TokenRegistryPlugin } from '@stove-labs/arbitrage-bot-token-registry';
+import { ExchangeVortexPlugin } from '@stove-labs/tezos-dex-vortex';
 
-const exchangeConfigQuipuswap1: ExchangePluginConfig = {
-  rpc: 'http://127.0.0.1:20000',
+const exchangeConfigQuipuswap: ExchangePluginConfig = {
+  rpc: 'https://mainnet.smartpy.io',
   identifier: 'QUIPUSWAP',
-  ecosystemIdentifier: 'TEZOS'
+  ecosystemIdentifier: 'TEZOS',
 };
 
-const exchangeConfigQuipuswap2: ExchangePluginConfig = {
-  rpc: 'http://127.0.0.1:20000',
-  identifier: 'QUIPUSWAP2',
-  ecosystemIdentifier: 'TEZOS'
+const exchangeConfigVortex: ExchangePluginConfig = {
+  rpc: 'https://mainnet.smartpy.io',
+  identifier: 'VORTEX',
+  ecosystemIdentifier: 'TEZOS',
 };
 
 const tokenList: TokenList = [
@@ -31,33 +32,45 @@ const tokenList: TokenList = [
   },
   {
     ticker: 'SMAK',
-    address: 'KT1...',
+    address: 'KT1TwzD6zV3WeJ39ukuqxcfK2fJCnhvrdN1X',
     decimals: 3,
     ecosystemIdentifier: 'TEZOS',
   },
   {
-    ticker: 'USD',
-    address: 'KT1WCcNF89rFB3jzhLJfeHh6yhSneMMu5Pb5',
-    decimals: 12,
+    ticker: 'kUSD',
+    address: 'KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV',
+    decimals: 18,
     ecosystemIdentifier: 'TEZOS',
   },
 ];
 
 const quipuswapList: ExchangeRegistry = [
   {
-    address: 'KT1ADHpHowU8nshdYj2gzwFoXiZgMG7PPk7A',
-    identifier: 'QUIPUSWAP1',
+    address: 'KT1Gdix8LoDoQng7YqdPNhdP5V7JRX8FqWvM',
+    identifier: 'QUIPUSWAP',
     ticker1: 'XTZ',
-    ticker2: 'USD',
+    ticker2: 'SMAK',
+  },
+  {
+    address: 'KT1K4EwTpbvYN9agJdjpyJm4ZZdhpUNKB3F6',
+    identifier: 'QUIPUSWAP',
+    ticker1: 'XTZ',
+    ticker2: 'kUSD',
   },
 ];
 
-const quipuswapList2: ExchangeRegistry = [
+const vortexList: ExchangeRegistry = [
   {
-    address: 'KT1RThWFuFHFqQxWEQZzQo2xGttB2ZMRvraZ',
-    identifier: 'QUIPUSWAP2',
+    address: 'KT1LzyPS8rN375tC31WPAVHaQ4HyBvTSLwBu',
+    identifier: 'VORTEX',
     ticker1: 'XTZ',
-    ticker2: 'USD',
+    ticker2: 'SMAK',
+  },
+  {
+    address: 'KT1Wjadao8AXkwNQmjstbPGtLd1ZrUyQEDX7',
+    identifier: 'VORTEX',
+    ticker1: 'XTZ',
+    ticker2: 'kUSD',
   },
 ];
 
@@ -65,32 +78,25 @@ const tokenRegistry = new TokenRegistryPlugin(tokenList);
 
 export const config: Config = {
   baseToken: {
-    ticker: 'USD',
+    ticker: 'XTZ',
   },
   quoteToken: {
-    ticker: 'XTZ',
+    ticker: 'SMAK',
   },
   plugins: {
     exchanges: [
-      new ExchangeQuipuswapPlugin(exchangeConfigQuipuswap1, quipuswapList),
-      new ExchangeQuipuswapPlugin(exchangeConfigQuipuswap2, quipuswapList2),
+      new ExchangeQuipuswapPlugin(exchangeConfigQuipuswap, quipuswapList),
+      new ExchangeVortexPlugin(exchangeConfigVortex, vortexList),
     ],
     token: tokenRegistry,
-    trigger: new TriggerIntervalPlugin({ interval: 5000 }),
+    trigger: new TriggerIntervalPlugin({ interval: 20000 }),
     reporter: new ConsoleReporterPlugin(),
     profitFinder: new ProfitFinderLitePlugin({
       tokenRegistry,
       profitSplitForSlippage: 0,
     }),
-    // accountants: deal with the balances in a plugin
-    // orchestrated by AccountantManager that does summing up of token balances across accountants
-    // [new AccountantTezosPlugin()]
     keychains: {
-      // !!! keychain is responsible for signing
       TEZOS: {},
-      // TEZOS: new KeychainTezosInMemoryPlugin({
-      //   privateKey: "...",
-      // }), // this plugin has a .sign(batchSwaps) or something...
     },
   },
 };
